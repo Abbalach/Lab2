c2c6a4ec202665d0f0f1d5138a72ce8cfd8e81b3
Abbalach <V.Dubinin.FIT.121.20@knute.edu.ua>
2021-09-21 20:24:36 +0300

    task5

---
+++ Lab2Task5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            user.login = "login";
+            user.name = "Felix";
+            user.secondname = "White";
+            user.age = 25;
+            Console.WriteLine("Login - " + user.login + ", name - " + user.name + ", secondname - " + user.secondname + ", age - " + user.age + ", date - " + user.date);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab2Task5/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2Task5
+{
+    class User
+    {
+        public string login { set; get; }
+        public string name { set; get; }
+        public string secondname { set; get; }
+        public int age { set; get; }
+        public DateTime date { get; }
+        public User()
+        {
+            date = DateTime.Now;
+        }
+    }
+}

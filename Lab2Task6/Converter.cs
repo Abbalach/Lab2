41d57291f679af09169ea2b6ac71c32dfcd50a43
Abbalach <V.Dubinin.FIT.121.20@knute.edu.ua>
2021-09-21 20:29:14 +0300

    task 6 commit

---
+++ Lab2Task6/Converter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2Task6
+{
+    class Converter
+    {
+        public double Usd { get; }
+        public double Eur { get; }
+        public double Rub { get; }
+        public Converter(double usd, double eur, double rub)
+        {
+            Usd = usd;
+            Eur = eur;
+            Rub = rub;
+        }
+        public double ConvertToUE(double count, string currtype)
+        {
+            switch (currtype)
+            {
+                case "usd":
+                    {
+                        return count * Usd;
+                    }
+                case "eur":
+                    {
+                        return count * Eur;
+                    }
+                case "rub":
+                    {
+                        return count * Rub;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Введено неправильное название валюты");
+                        return 0;
+                    }
+            }
+        }
+        public double ConvertFromUE(double count, string currtype)
+        {
+            switch (currtype)
+            {
+                case "usd":
+                    {
+                        return count / Usd;
+                    }
+                case "eur":
+                    {
+                        return count / Eur;
+                    }
+                case "rub":
+                    {
+                        return count / Rub;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Введено неправильное название валюты");
+                        return 0;
+                    }
+            }
+        }
+    }
+}
---
+++ Lab2Task6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Converter converter = new Converter(26.69, 31.29, 0.37);
+            double curr = 1000;
+            Console.WriteLine("Из евро в гривны " + converter.ConvertToUE(curr, "eur") + ", из гривны в евро " + converter.ConvertFromUE(curr, "eur"));
+            Console.WriteLine("Из долларов в гривны " + converter.ConvertToUE(curr, "usd") + ", из гривны в доллары " + converter.ConvertFromUE(curr, "usd"));
+            Console.WriteLine("Из рублей в гривны " + converter.ConvertToUE(curr, "rub") + ", из гривны в рубли " + converter.ConvertFromUE(curr, "rub"));
+            Console.ReadKey();
+        }
+    }
+}

78c02244ab11a99b2749859b81f76890bc1f5bc0
Abbalach <V.Dubinin.FIT.121.20@knute.edu.ua>
2021-09-21 20:09:45 +0300

    task 3 commit

---
+++ Lab2Task3/Book.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2Task3
+{
+    class Book
+    {
+        public Title CurrTitle { get; }
+        public Author CurrAuthor { get; }
+        public Content CurrContent { get; }
+        public Book(string title, string author, string content)
+        {
+            CurrTitle = new Title();
+            CurrAuthor = new Author();
+            CurrContent = new Content();
+            CurrTitle.title = title;
+            CurrAuthor.author = author;
+            CurrContent.content = content;
+        }
+        public Book()
+        {
+            CurrTitle = new Title();
+            CurrAuthor = new Author();
+            CurrContent = new Content();
+        }
+        public class Title
+        {
+            public string title = "";
+            public void Show()
+            {
+                Console.WriteLine("Title: " + title);
+            }
+        }
+        public class Author
+        {
+            public string author= "";
+            public void Show()
+            {
+                Console.WriteLine("Author: " + author);
+            }
+        }
+        public class Content
+        {
+            public string content = "";
+            public void Show()
+            {
+                Console.WriteLine("Content: " + content);
+            }
+        }
+    }
+}
---
+++ Lab2Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("The Book", "Ben Zucker", "some text...");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            book.CurrTitle.Show();
+            Console.ForegroundColor = ConsoleColor.Red;
+            book.CurrAuthor.Show();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            book.CurrContent.Show();
+            Console.ReadKey();
+        }
+    }
+}

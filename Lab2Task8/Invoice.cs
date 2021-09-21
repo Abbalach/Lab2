b85cf510d2347b1beb46e1791bdfd8c228da758a
Abbalach <V.Dubinin.FIT.121.20@knute.edu.ua>
2021-09-21 20:37:35 +0300

    task 8 commit

---
+++ Lab2Task8/Invoice.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2Task8
+{
+    class Invoice
+    {
+        public string Customer { get; }
+        public string Provider { get; }
+        public int Account { get; }
+        private string Article { set; get; }
+        private int Quantity { set; get; }
+        private double ArticlePrice { get; set; }
+        private double ProductPrice { get; set; }
+        private double VAT { get; }
+        public Invoice(string customer, string provider, int account)
+        {
+            Customer = customer;
+            Provider = provider;
+            if (account <= 0)
+            {
+                Console.WriteLine("Неправильно введен счет.");
+            }
+            else
+                Account = account;
+            VAT = 0.2;
+        }
+        public void SetArticle(string articlename, int articlecount, double price)
+        {
+            Article = articlename;
+            Quantity = articlecount;
+            ProductPrice = price;
+        }
+        public void OrderWithVAT()
+        {
+            ArticlePrice = ProductPrice * Quantity * (1 - VAT);
+            if (Account > ArticlePrice)
+            {
+                Console.WriteLine("Со счета " + Customer + " компания " + Provider + " сняла " + ArticlePrice + " грн. Заказ был оформлен.");
+            }
+            else
+            {
+                Console.WriteLine("На вашем счете недостаточно денег (стоимость заказа " + ArticlePrice + " грн.)");
+            }
+        }
+        public void OrderWithoutVAT()
+        {
+            ArticlePrice = ProductPrice * Quantity;
+            if (Account > ArticlePrice)
+            {
+                Console.WriteLine("Со счета " + Customer + " компания " + Provider + " сняла " + ArticlePrice + " грн. Заказ был оформлен.");
+            }
+            else
+            {
+                Console.WriteLine("На вашем счете недостаточно денег (стоимость заказа " + ArticlePrice + " грн.)");
+            }
+        }
+    }
+}
---
+++ Lab2Task8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice invoice = new Invoice("Bill White", "DistributionUA", 10000);
+            invoice.SetArticle("Принтер", 6, 1800);
+            invoice.OrderWithVAT();
+            invoice.OrderWithoutVAT();
+            Console.ReadKey();
+        }
+    }
+}

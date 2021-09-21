72c2682ab9deeee630b533c84f092b3e87a51937
Abbalach <V.Dubinin.FIT.121.20@knute.edu.ua>
2021-09-21 20:56:52 +0300

    task 2 commit

---
+++ Lab2Task2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первую сторону");
+            var side1 = Console.ReadLine();
+            Console.WriteLine("Введите вторую сторону");
+            var side2 = Console.ReadLine();
+            bool helper = true;
+            Rectangle rectangle = null;
+            try
+            {
+                rectangle = new Rectangle(Convert.ToDouble(side1), Convert.ToDouble(side2));
+            }
+            catch 
+            {
+                Console.WriteLine("Вы ввели параметры неправильно.");
+                helper = false;
+            }
+            if (helper)
+            {
+                Console.WriteLine("Площадь прямоугольника - " + rectangle.AreaCalculator());
+                Console.WriteLine("Периметр прямоугольника - " + rectangle.PerimeterCalculator());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab2Task2/Rectangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2Task2
+{
+    class Rectangle
+    {
+        private double Side1 { set; get; }
+        private double Side2 { set; get; }
+        public double Area { get; }
+        public double Perimeter { get; }
+        public Rectangle(double side1, double side2)
+        {
+            Side1 = side1;
+            Side2 = side2;
+            Perimeter = 2 * (Side1 + Side2);
+            Area = Side1 * Side2;
+        }
+        public double PerimeterCalculator()
+        {
+            return 2 * (Side1 + Side2);
+        }
+        public double AreaCalculator()
+        {
+            return Side1 * Side2;
+        }
+    }
+}

35c5961136347f3ed4580cf83ed69ebdef0a631e
Abbalach <V.Dubinin.FIT.121.20@knute.edu.ua>
2021-09-21 18:37:09 +0300

    initial

---
+++ Lab2Task1/Address.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2Task1
+{
+    class Address
+    {
+        public int Index { set; get; }
+        public string Country { set; get; }
+        public string City { set; get; }
+        public string Street { set; get; }
+        public string House { set; get; }
+        public int Apartment { set; get; }
+    }
+}
---
+++ Lab2Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Address address = new Address();
+            address.Index = 466541;
+            address.Country = "Ukraine";
+            address.City = "Kiyv";
+            address.Street = "Vladimirskaya";
+            address.House = "49a";
+            address.Apartment = 12;
+            Console.WriteLine("Index = " + address.Index + ", country = " + address.Country + ", city = " 
+                + address.City + ", street = " + address.Street + ", house = " + address.House + ", apartment = " + address.Apartment);
+            Console.ReadKey();
+        }
+    }
+}
